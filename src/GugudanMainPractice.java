57c3ea5e62435253e47a303fc1ff7ece41350112
NamKiHyeok <NamKiHyeok@DESKTOP-V8FTS1P>
2021-09-14 09:48:07 +0900

    연습 클래스 추가

---
+++ src/GugudanMainPractice.java
@@ -0,0 +1,9 @@
+
+public class GugudanMainPractice {
+	public static void main(String[] args) {
+		for(int i =2; i < 10; i++) {
+			int[] result = MethodGugudan.calculate(i);
+			MethodGugudan.print(result);
+		}
+	}
+}
