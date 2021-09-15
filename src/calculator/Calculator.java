cbf544986b2340d5ecd037fefbdb3c6f667a23c5
NamKiHyeok <NamKiHyeok@DESKTOP-V8FTS1P>
2021-09-15 15:34:13 +0900

    2개의 숫자값을 받아 사친연산 계산

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,11 +1,24 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 	public static void main(String[] args) {
-		System.out.println("덧셈 : " + (3 + 4));
-		System.out.println("뺼셈 : " + (4 - 3));
-		System.out.println("곱셈 : " + (4 * 3));
-		System.out.println("나눗셈 : " + (4 / 2));
+		
+		//사용자로부터 2개의 숫자 값을 입력 받은 후 2개의 숫자 값을 활용해 사칙연산을 계산하라.
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("첫 번쨰 입력 값: ");
+		int first = scanner.nextInt();
+		System.out.println(first);
+		
+		System.out.println("두 번쨰 입력 값: ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		
+		System.out.println("덧셈 : " + (first + second));
+		System.out.println("뺄셈 : " + (first - second));
+		System.out.println("곱셈 : " + (first * second));
+		System.out.println("나눗셈 : " + (first / second));
 		
 	}
 }
