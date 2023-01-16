3ec1de7f6d23ffad6903821da4aef244b1e0761d
carpe <carpe@User>
2023-01-16 15:02:22 +0900

    4-11

---
+++ src/Chapter4/exam4_11.java
@@ -0,0 +1,20 @@
+package Chapter4;
+
+public class exam4_11 {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		int num2 = 1;
+		int num3 = 0;
+		System.out.println(num1+","+num2); 
+		
+		for (int i = 0; i < 8; i++) {
+			//num3의 값을 구한다.
+			//num3을 출력한다. 
+			 
+			//두 번째 수열을 첫 번째 값으로 한다
+			//세 번쨰 수열을 두 번째 값으로 한다.			
+		}
+	}
+
+}
