77b03a425cb5e04c4c203f4819b6049a37a86d67
KiHyoek Nam <carpediem7490@gmail.com>
2023-02-14 12:22:29 +0900

    exam5-6

---
+++ src/Chapter5/exam5_6.java
@@ -0,0 +1,17 @@
+package Chapter5;
+
+public class exam5_6 {
+
+	public static void main(String[] args) {
+		int[] coinUnit = {500, 100, 50, 10};
+		
+		int money = 2680;
+		System.out.println("money=" +money);
+		
+		for(int i=0; i<coinUnit.length; i++) {
+			 // money를 각 코인(500,100,50,10)이 몇개로 나누면 되는지
+			 // money를 코인으로 나누고 남은 금액
+		}
+	}
+
+}
