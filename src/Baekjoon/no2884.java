1334f14c0d5cd597238cc490e29681941b64c8d5
KiHyoek Nam <carpediem7490@gmail.com>
2023-02-27 16:10:44 +0900

    백준 no2884 수정

--- src/Baekjoon/no2884.java
+++ src/Baekjoon/no2884.java
@@ -10,16 +10,21 @@ public class no2884 {
 		int M = sc.nextInt();
 		sc.close();
 		
+		// 분이 45분 보다 작다면
 		if(M<45) {
+			// 시간은 -1
 			H--;
-			M=60 - (45 - M);
-			if(H < 0) {
+			// 분은 +15
+			M = M+15;
+			
+			// 시간이 0시라면
+			// 45분 전 시간에는 23시이다.
+			if(H<0) {
 				H = 23;
 			}
-			System.out.println(H+""+M);
-		}
-		else {
-			System.out.println(H+""+(M-45));
+			System.out.println(H+" "+M);
+		} else {
+			System.out.println(H+ " " + (M - 45));
 		}
 	}
 
