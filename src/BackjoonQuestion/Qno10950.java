5b9136ca12f125f0abf3a71487d14084bf8ee1a0
KiHyoek Nam <carpediem7490@gmail.com>
2023-03-06 10:30:32 +0900

    백준 no10950 문제

---
+++ src/BackjoonQuestion/Qno10950.java
@@ -0,0 +1,24 @@
+package BackjoonQuestion;
+
+
+import java.util.Scanner;
+
+public class Qno10950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// test케이스 갯수
+		// 더하기 결과값을 담을 배열 
+		
+
+		
+		sc.close();
+		
+		// 변수 k에 배열 값을 하나씩 담는다.
+		// 출력한다.
+
+	}
+
+}
+
