b6ceee2661cedaa426f9af8c42c95a5d7fdc5f6a
KiHyoek Nam <carpediem7490@gmail.com>
2023-03-07 10:22:29 +0900

    백준 no.11021

---
+++ src/BackjoonQuestion/Qno11021.java
@@ -0,0 +1,18 @@
+package BackjoonQuestion;
+
+import java.util.Scanner;
+
+public class Qno11021 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt(); // 테스트 케이스 개수
+		
+		//테스트 케이스만큼 숫자를 입력받은 후 출력한다.
+
+		
+		sc.close();
+	}
+
+}
