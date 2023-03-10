1d28f12d43bb0c38503b10b36813c8b2e0f81cc9
KiHyoek Nam <carpediem7490@gmail.com>
2023-03-10 11:17:04 +0900

    백준 no.10951

---
+++ src/BaekjoonAnswer/no10951.java
@@ -0,0 +1,19 @@
+package BaekjoonAnswer;
+
+import java.util.Scanner;
+
+public class no10951 {
+
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			
+			while(sc.hasNextInt()) {
+				
+				int A = sc.nextInt();
+				int B = sc.nextInt();
+
+				System.out.println(A+B);
+			}
+			sc.close();
+		}
+}
